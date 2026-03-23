2b22008f5c5538b10be524cbca5fc55e8afdafe7
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-03-22 21:18:57 -0500

    Setup for Unit 1 project

---
+++ bakers-blog/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Baker's Blog</h1>
+        <div className="hero">
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
