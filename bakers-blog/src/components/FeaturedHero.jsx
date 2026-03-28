763ad72d5bff35534edce783839633fcc022d8ea
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-03-28 13:52:27 -0500

    WIP on hero image on home page

--- bakers-blog/src/components/FeaturedHero.jsx
+++ bakers-blog/src/components/FeaturedHero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './css/FeaturedHero.css'
 
 export default function FeaturedHero() {
   return (
--- bakers-blog/src/pages/Home.jsx
+++ bakers-blog/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router';
+import FeaturedHero from '../components/FeaturedHero';
 
 function Home() {
 
     return (
         <div>
-            <h2>Home page</h2>
+            <FeaturedHero />
         </div>
     )
 }
