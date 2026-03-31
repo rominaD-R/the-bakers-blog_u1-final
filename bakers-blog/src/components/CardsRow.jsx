388fd272ff5c11a22ae681e46dcbbfb70a205ef1
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-03-30 22:16:07 -0500

    Forogt to make branch again

---
+++ bakers-blog/src/components/CardsRow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './css/CardsRows.css'
+
+export default function CardsRow() {
+  return (
+    <div className='cards-row'>
+        cards go here
+    </div>
+  )
+}
--- bakers-blog/src/pages/Home.jsx
+++ bakers-blog/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router';
 import FeaturedHero from '../components/FeaturedHero';
+import CardsRow from '../components/CardsRow';
 
 function Home() {
 
     return (
         <div>
             <FeaturedHero />
+            <div className='text-cont'>
+                <h2>All Time Favorites</h2>
+                <CardsRow />
+            </div>
         </div>
     )
 }
