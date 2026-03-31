b144517d883366fa61bd83b0b4bc11bd5dad5ed5
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-03-30 21:37:27 -0500

    Made feature hero part

--- bakers-blog/src/components/FeaturedHero.jsx
+++ bakers-blog/src/components/FeaturedHero.jsx
@@ -5,11 +5,17 @@ export default function FeaturedHero() {
   return (
     <>
         <div id='featuredHero'>
-            <div className='text-feature'>
-                <h2>Featured Recipe!</h2>
+            <div className='white-block'>
+              <div className='text-feature'>
+                <h2>Pizza Margarita</h2>
                 <h4>About the recipe:</h4>
-                <p>Intro text.</p>
-                <button>Read More</button>
+                <p>
+                  This is the best margherita pizza that you’ll be able to make in your own 
+                  kitchen, with a standard oven, with normal everyday ingredients (no fancy flours required), and without preparing an 
+                  incredibly time-intensive pizza dough days in advance.
+                  </p>
+                <button>See Full Recipe</button>
+              </div>
             </div>
         </div>
     </>
