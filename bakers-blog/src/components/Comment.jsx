257571f12e5c24522d650c49ebfbd51055d48955
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-04-04 14:41:33 -0500

    WIP on adding comments to recipe

---
+++ bakers-blog/src/components/Comment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Comment() {
+  return (
+    <div>
+        Comment
+        <button>Delete</button>
+        <button>Edit</button>
+    </div>
+  )
+}
--- bakers-blog/src/pages/IndividualRecipe.jsx
+++ bakers-blog/src/pages/IndividualRecipe.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams, Link } from 'react-router';
 import { recipeMockData } from '../data/recipes'
 import './IndividualRecipe.css'
@@ -11,6 +11,18 @@ export default function IndividualRecipe() {
 
     console.log(id);
     console.log(currentRecipe);
+
+    const [commentData, setCommentData] = useState([...currentRecipe.comments]);
+
+
+    const addComment = (e) => {
+        e.preventDefault();
+        // const { name, value } = e.target; 
+        const currentComment = document.getElementById("commentText").value;
+        console.log("Comment added:  " + currentComment);
+        setCommentData([...commentData, currentComment]);
+        console.log(commentData);
+    }
 
     return (
         <div className='text-cont individual-recipe-page'>
@@ -41,12 +53,12 @@ export default function IndividualRecipe() {
             <div className='comments-section'>
                 <hr />
                 <h4>Comments</h4>
-                {currentRecipe.comments.map((comment) => <div className='comment'>{comment}</div>)}
+                {commentData.map((comment) => <div className='comment'>{comment}</div>)}
                 <form id='commentForm' action="">
                     <b>Add your comment!</b>
                     <div>
                         <textarea name="comment" id="commentText"></textarea>
-                        <button id='submitComment'>Post Comment</button>
+                        <button onClick={addComment} id='submitComment'>Post Comment</button>
                     </div>                    
                 </form>
             </div>
